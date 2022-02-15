85aea91f013f635f0b6d6f10831d342dd3d825bc
5angjae <sangjea5@naver.com>
2022-02-15 17:57:58 +0900

    C# fastcampus & ASP.NET Core 220215

---
+++ ASP .NET Core/TEST/BookAPI/BookAPI/Repositories/BookRepository.cs
@@ -0,0 +1,43 @@
+﻿using BookAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BookContext context;
+
+        public BookRepository(BookContext context)
+        {
+            this.context = context;
+        }
+
+        public Task<Book> Create(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Book>> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Book> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
