936132a5eb79019816998dcc72a403065c00b08f
5angjae <sangjea5@naver.com>
2022-02-17 17:56:41 +0900

    C# data type -ing

---
+++ C#/Code/TEST/FastCampus_Sample_CS/024_Data_cast/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+/*-----------------------------------------------------------------------------
+ * Name: _024_Data_cast
+ * DESC: cast연산자(  ) 기초
+-----------------------------------------------------------------------------*/
+namespace _024_Data_cast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 100;
+            double dNum = (double)num;
+            Console.WriteLine("num: {0},  dNum: {1}", num, dNum);
+
+            double dNum2 = 12345678912;
+            int num2 = (int)dNum2; //큰데이터형을 작은 데이터형으로 변경시 주의!!!
+            Console.WriteLine("dNum2: {0},  num2: {1}", dNum2, num2);
+        }
+    }
+}
