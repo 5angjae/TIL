46d716500681d55f6c10a3f5bba065b80606874d
5angjae <sangjea5@naver.com>
2022-02-18 18:01:36 +0900

    C# basic if ~ for ~ while

---
+++ C#/Code/TEST/FastCampus_Sample_CS/055_Check/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _055_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int a, b;
+            
+
+            for(int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("{0}: 다음 두 수의 합은 몇? (총 5문제)", i);
+                a = rand.Next(1, 100);
+                b = rand.Next(1, 100);
+
+            }
+        }
+    }
+}
